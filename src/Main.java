3fcec6beeb9ff85c81470900a287d79e90ae7c67
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-10 14:59:23 +0300

    added giving a name through a constructor

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Cat myCat1 = new Cat();
+        Cat myCat1 = new Cat("Charlie");
         myCat1.sayHello();
     }
 }
 
 class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
     void sayHello() {
         System.out.println("Hello!");
     }
 }
 
 class Cat extends Animal {
+
+    Cat(String name) {
+        super(name);
+    }
+
     @Override
     void sayHello() {
         System.out.println("Meow!");
