1da8117f9b8424a52baa42d41d2a8d5d9b979301
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-10 15:33:03 +0300

    calling a method without creating an example of exterior class

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Cat myCat1 = new Cat("Charlie");
-        System.out.println(myCat1.name);
-        myCat1.sayHello();
+        Library.Archive.readParchment();
     }
 }
 
-class Animal {
-    String name;
-
-    Animal(String name) {
-        this.name = name;
-    }
-
-    void sayHello() {
-        System.out.println("Hello!");
-    }
-}
-
-class Cat extends Animal {
-
-    Cat(String name) {
-        super(name);
-    }
-
-    @Override
-    void sayHello() {
-        System.out.println("Meow!");
+class Library {
+    static class Archive {
+        static void readParchment() {
+            System.out.println("Ancient parchment was found out");
+        }
     }
 }
