fbf67a3ccf194c814ee424a7a53780d25bf709cd
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-11 22:19:22 +0300

    HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+
 
 public class Main {
     public static void main(String[] args) {
-        List<String> fruits = new ArrayList<>();
-        fruits.add("Яблоко");
-        fruits.add("Банан");
-        fruits.add("Груша");
+        HashMap<String, Integer> studentCard = new HashMap<String, Integer>();
+        studentCard.put("Иван", 20);
+        studentCard.put("Мария", 21);
+        studentCard.put("Алексей", 19);
 
-        System.out.println(fruits);
+        System.out.println(studentCard);
     }
 }
