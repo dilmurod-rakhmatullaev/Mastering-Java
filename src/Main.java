0866cc4984467b492507d36d988d8b1912629303
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-13 12:36:18 +0300

    applying removeIf and Iterator.remove

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> tasks = new LinkedList<>();
+        List<Integer> numbers = new ArrayList<>(List.of(1, -2, 3, -4, 5));
+        List<Integer> digits = new ArrayList<>(List.of(-6, 7, -8, 9, 0));
 
-        tasks.add(5);
-        tasks.add(15);
-        tasks.add(25);
+        numbers.removeIf(n -> n < 0);
+        System.out.println(numbers);
 
-        System.out.println(tasks.peek());
-        System.out.println(tasks.size());
+        Iterator<Integer> it = digits.iterator();
+        while (it.hasNext()) {
+            int n = it.next();
+            if (n < 0) {
+                it.remove();
+            }
+        }
+        System.out.println(digits);
     }
 }
