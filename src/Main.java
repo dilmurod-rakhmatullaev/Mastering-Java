1341568d5976acd500fe8ceedfdf504106edf09c
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-15 22:36:37 +0300

    first task with Stream api

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> animals = new ArrayList<>();
-        animals.add("cat");
-        animals.add("dog");
-        animals.add("bird");
-        animals.add("fish");
-
-        // Получаем ListIterator для безопасного прохода и модификации списка
-        ListIterator<String> iterator = animals.listIterator();
-
-        // Итерируемся по списку
-        while (iterator.hasNext()) {
-            String animal = iterator.next();
-            if (animal.length() == 3) {
-                iterator.remove();
-                iterator.add("pet");
-            }
-        }
-
-
-        // После обработки выводим итоговый список на экран
-        System.out.println(animals);
+        int[] levels = {1, 2, 3, 4, 5};
+        Arrays.stream(levels).forEach(System.out::println);
     }
 }
