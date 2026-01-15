95262facbe261965519159289bafa8098cf966fd
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-15 22:41:18 +0300

    Collections and stream api

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        int[] levels = {1, 2, 3, 4, 5};
-        Arrays.stream(levels).forEach(System.out::println);
+        List<String> categories = Arrays.asList("java", "stream", "api");
+        List<String> uppercased = categories.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(uppercased);
     }
 }
