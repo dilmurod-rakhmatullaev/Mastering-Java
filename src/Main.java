7bd854e02f78cb3134e9b24936389bd59846064b
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-16 23:38:01 +0300

    .filter, .peek, .map and .collect

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> students = List.of(
-                "Анна", "Борис", "Алексей", "Мария", "Андрей", "Виктор", "Артур", "Галина", "Алиса", "Дмитрий"
-        );
-        students.stream()
-                .filter(n -> n.startsWith("А"))
-                .forEach(System.out::println);
+        List<Integer> readings = Arrays.asList(10, 15, 20, 25, 30);
+        List<String> report = readings.stream()
+                .filter(n -> n % 10 == 0)
+                .peek(n -> System.out.println("Значение " + n + " прошло проверку"))
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+
+        System.out.println(report);
     }
 }
