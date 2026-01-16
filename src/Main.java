bcc62ce7e18773f747cd788ba37c8fba13ece4c3
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-16 23:31:39 +0300

    .filter and .forEach(System.out::println)

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> sales = List.of(5, 7, 5, 9, 7, 11, 13, 9, 15);
-        List<Integer> top4Unique = sales.stream()
-                        .distinct()
-                                .limit(4)
-                                        .collect(Collectors.toList());
-
-        System.out.println(top4Unique);
+        List<String> students = List.of(
+                "Анна", "Борис", "Алексей", "Мария", "Андрей", "Виктор", "Артур", "Галина", "Алиса", "Дмитрий"
+        );
+        students.stream()
+                .filter(n -> n.startsWith("А"))
+                .forEach(System.out::println);
     }
 }
