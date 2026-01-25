ff2383c87108f2c0d09523905f6f63a09b08e3af
Dilmurod Rakhmatullaev <dilmurodrakhmatullayev@gmail.com>
2026-01-25 22:58:04 +0300

    setting delimiter with collect

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
-        Optional<Integer> sum1 = numbers.stream()
-                .reduce(Integer::sum);
-
-        System.out.println(sum1);
-
-        int sum2 = numbers.stream()
-                .reduce(0, (a, b) -> a + b);
-
-        System.out.println(sum2);
+        List<String> magicWords = List.of("Java", "Stream", "API");
+        String spell = magicWords.stream()
+                .collect(Collectors.joining(" "));
+        System.out.println(spell);
     }
 }
